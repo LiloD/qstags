86927902538875e1e28b5e3ab41af1ee94ec472c
LiloD <zhizhuoding@gmail.com>
2015-12-08 22:30:40 -0500

    initial push

---
+++ public/javascripts/app.js
@@ -0,0 +1,18 @@
+(function(){
+    var app = angular.module('main', []);
+
+    app.controller('tagsCtl', ['$http', '$scope', function($http, $scope){
+        $scope.tags = ['c', 'c++', 'opengl', 'STL'];
+        $scope.selectedTags = [];
+
+        $scope.appendTo = function(tag){
+            if(!!tag && $scope.selectedTags.indexOf(tag) < 0) 
+                $scope.selectedTags.push(tag);
+        }
+
+        $scope.remove = function(idx){
+            $scope.selectedTags.slice(idx, 1);
+        }
+    }]);
+
+})();
